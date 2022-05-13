f3cbcf44030e75da8f3757819e38e48885570f77
Ph0enixKM <pkaras.it@gmail.com>
2022-05-13 22:00:47 +0200

    fix: typescript errors

--- src/components/atoms/donate/ApplePay.vue
+++ src/components/atoms/donate/ApplePay.vue
@@ -1,6 +1,14 @@
 <script setup lang="ts">
 import { reactive, onMounted } from 'vue'
-import { createPayment } from '@/util/rest.ts'
+import { createPayment } from '@/util/rest'
+
+// Extend window type with global Stripe
+declare global {
+    interface Window {
+        Stripe: any
+    }
+}
+
 const stripe = window.Stripe('pk_test_51KyzN8CF8LXgMIUQOHz3B5xOQrRHYPe8frOvjL7NIRLtYB3LBXjUOjA3JnqifyFttvYKhnpB54FPlYYuEcnEc3mg008HErZxiE')
 const amount = 99
 const paymentRequest = stripe.paymentRequest({
@@ -24,7 +32,7 @@ onMounted(async () => {
     const canPay = await paymentRequest.canMakePayment()
     if (canPay) paymentButton.mount('.apple-pay-donate-request')
     else cannotPay.value = true;
-    paymentRequest.on('paymentmethod', async (e) => {
+    paymentRequest.on('paymentmethod', async (e: any) => {
         const { clientSecret } = await createPayment({ amount })
         const { error, paymentIntent } = await stripe.confirmCardPayment(clientSecret, {
             payment_method: e.paymentMethod.id
@@ -37,7 +45,7 @@ onMounted(async () => {
         }
         e.complete('success')
         if (paymentIntent.status === 'requires_action') {
-            stripe.confirmCardPayment(secret)
+            stripe.confirmCardPayment(clientSecret)
         }
     })
 })
